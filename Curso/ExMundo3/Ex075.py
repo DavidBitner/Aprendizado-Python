29ebca279c5da61873f58a8014ac9441c35e64b9
David <david-bitner@hotmail.com>
2020-07-31 09:52:04 -0300

    Update Ex075.py
    Corrigido problema que surgia quando o numero 3 não era parte das entradas

--- Curso/ExMundo3/Ex075.py
+++ Curso/ExMundo3/Ex075.py
@@ -13,5 +13,10 @@ for c in tupla:
 
 print(f'Você digitou os valores {tupla}')
 print(f'O numero 9 aparece {tupla.count(9)} vezes')
-print(f'O valor 3 apareceu na {tupla.index(3) + 1}º posição')
+
+if 3 in tupla:
+    print(f'O valor 3 apareceu na {tupla.index(3) + 1}º posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+
 print(f'Os valores pares digitados foram {pares}')
