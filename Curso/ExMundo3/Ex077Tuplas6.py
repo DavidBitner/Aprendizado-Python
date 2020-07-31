b5f9fc6c9c992c1d3af673b5988a1b7e1158d583
David <david-bitner@hotmail.com>
2020-07-31 10:29:54 -0300

    Create Ex077Tuplas6.py

---
+++ Curso/ExMundo3/Ex077Tuplas6.py
@@ -0,0 +1,8 @@
+palavras = ('Doidera', 'Calipso', 'Yoda', 'Axt', 'Jovirone', 'Matilda', 'Schwarzenneger', 'Mustefaga', 'Instinct', 'Kobayashi', 'Ludgero', 'Salcicha', 'Scooby', 'Turtle', 'Lily', 'Toast')
+
+for palavra in palavras:
+    print(f'Na palavra {palavra.upper()} temos ', end='')
+    for c in range(0, len(palavra)):
+        if palavra[c] in 'AaEeIiOoUu':
+            print(palavra[c].lower(), end=' ')
+    print()
