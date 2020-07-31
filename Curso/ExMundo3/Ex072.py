f808789b91cbde7486f0a4141b5e168d23bc3fe5
David <david-bitner@hotmail.com>
2020-07-31 08:46:34 -0300

    Update Ex072.py

--- Curso/ExMundo3/Ex072.py
+++ Curso/ExMundo3/Ex072.py
@@ -1,5 +1,10 @@
 numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze,', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesete', 'Dezoito', 'Dezenove', 'Vinte')
 
-entrada = int(input('Digite um numero entre 0 e 20: '))
+while True:
+    entrada = int(input('Digite um numero entre 0 e 20: '))
+    if entrada > 20 or entrada < 0:
+        print('Tente novamente. ', end='')
+    else:
+        break
 
 print(f'O numero {entrada} por extenso é {numeros[entrada]}')
