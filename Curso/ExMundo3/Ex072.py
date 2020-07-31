eab5ce8f6619c04da3d6dcec358fb7278265434d
David <david-bitner@hotmail.com>
2020-07-31 08:42:52 -0300

    Update Ex072.py

---
+++ Curso/ExMundo3/Ex072.py
@@ -0,0 +1,5 @@
+numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze,', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesete', 'Dezoito', 'Dezenove', 'Vinte')
+
+entrada = int(input('Digite um numero entre 0 e 20: '))
+
+print(f'O numero {entrada} por extenso é {numeros[entrada]}')
