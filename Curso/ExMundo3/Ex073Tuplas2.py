e66ffc10e78219533abba5e665d3453bf4c280fe
David <david-bitner@hotmail.com>
2020-08-02 06:59:50 -0300

    Update Ex073Tuplas2.py
    Adicionado comando esquecido e melhorado o ultimo comando

--- Curso/ExMundo3/Ex073Tuplas2.py
+++ Curso/ExMundo3/Ex073Tuplas2.py
@@ -5,6 +5,9 @@ posicaoChapecoense = campeonato.index('Chapecoense') + 1
 print('\nEstes são todos os times do campeonato Brasileiro de futebol:')
 print(campeonato)
 
+print('\nEstes são os 5 primeiros times na tabela:')
+print(campeonato[0:5])
+
 print('\nOs ultimos 4 colocados da tabela são:')
 print(campeonato[-4:])
 
@@ -12,3 +15,5 @@ print('\nA lista de times na ordem alfabética é:')
 print(sorted(campeonato))
 
 print(f'\nO time Chapecoense se encontra na {posicaoChapecoense}º posição')
+
+print(f'\nO time São Paulo se encontra na {campeonato.index("São Paulo") + 1}º posição')
