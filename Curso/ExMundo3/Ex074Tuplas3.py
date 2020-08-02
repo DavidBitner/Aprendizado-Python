2fa580e2c116beb6b74adc31860d1061325e41c6
David <david-bitner@hotmail.com>
2020-08-02 07:04:54 -0300

    Update Ex074Tuplas3.py

--- Curso/ExMundo3/Ex074Tuplas3.py
+++ Curso/ExMundo3/Ex074Tuplas3.py
@@ -7,16 +7,16 @@ for c in range(0, 10):
     tupla[c] = random.randint(0, 11)
 '''
 
-maior = 0
-menor = 0
+'''maior = 0
+menor = 0'''
 
-a = random.randint(0, 10)
+'''a = random.randint(0, 10)
 c = random.randint(0, 10)
 d = random.randint(0, 10)
 e = random.randint(0, 10)
-b = random.randint(0, 10)
+b = random.randint(0, 10)'''
 
-tupla = (a, b, c, d, e)
+tupla = (random.randint(0, 10), random.randint(0, 10), random.randint(0, 10), random.randint(0, 10), random.randint(0, 10))
 
 print('Os valores sorteados foram: ', end='')
 for contador in range(0, len(tupla)):
@@ -24,7 +24,7 @@ for contador in range(0, len(tupla)):
 
 print()
 
-for contador in range(0, len(tupla)):
+'''for contador in range(0, len(tupla)):
     if contador == 0:
         maior = tupla[contador]
         menor = tupla[contador]
@@ -33,7 +33,7 @@ for contador in range(0, len(tupla)):
             maior = tupla[contador]
         
         if tupla[contador] < menor:
-            menor = tupla[contador]
+            menor = tupla[contador]'''
 
-print(f'O maior valor sorteado foi o {maior}')
-print(f'O menor valor sorteado foi o {menor}')
+print(f'O maior valor sorteado foi o {max(tupla)}')
+print(f'O menor valor sorteado foi o {min(tupla)}')
