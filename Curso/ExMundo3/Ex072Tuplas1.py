188e9237d24909d3e3c0e4a12e6bb3b073f637aa
David <david-bitner@hotmail.com>
2020-08-02 06:51:58 -0300

    Update Ex072Tuplas1.py
    Exercicio melhorado para ser possível realizar mais de uma consulta

--- Curso/ExMundo3/Ex072Tuplas1.py
+++ Curso/ExMundo3/Ex072Tuplas1.py
@@ -1,10 +1,18 @@
 numeros = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze,', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesete', 'Dezoito', 'Dezenove', 'Vinte')
+novamente = ' '
 
 while True:
     entrada = int(input('Digite um numero entre 0 e 20: '))
     if entrada > 20 or entrada < 0:
         print('Tente novamente. ', end='')
     else:
-        break
+        print(f'O numero {entrada} por extenso é {numeros[entrada]}')
 
-print(f'O numero {entrada} por extenso é {numeros[entrada]}')
+        while True:
+            novamente = str(input('Deseja tentar novamente? [S/N]')).strip().lower()
+            if novamente not in 'sn':
+                print('Invalido, tente novamente!')
+            else:
+                break
+    if novamente in 'n':
+        break
