209b73f473dcd6e9de89de6f3258e4e2e37896f8
David <david-bitner@hotmail.com>
2020-08-03 08:14:51 -0300

    Update Ex078Listas1.py

--- Curso/ExMundo3/Ex078Listas1.py
+++ Curso/ExMundo3/Ex078Listas1.py
@@ -7,13 +7,6 @@ menor = 0
 for c in range(0, 5):
     lista.append(int(input(f'Digite um valor para a posição {c}: ')))
 
-print(f'Você digitou os valores: {lista}')
-
-'''print(f'O maior valor está na posição {lista.index(max(lista)) + 1} e foi o {max(lista)}')
-
-print(f'O menor valor está na posição {lista.index(min(lista)) + 1} e foi o {min(lista)}')
-'''
-
 for c, valor in enumerate(lista):
     if maior == 0:
         maior = valor
@@ -34,7 +27,9 @@ for c, valor in enumerate(lista):
         posicaoMenor.append(c)
     elif valor == menor:
         posicaoMenor.append(c)
-    
+
+print(f'Você digitou os valores: {lista}')
+
 if len(posicaoMaior) > 1:
     print(f'O maior valor digitado foi o {maior} e ele se encontra nas posições ', end='')
     for valores in posicaoMaior:
