e026cef61ab8e3c0609a5a5896e0b03eb757c256
David <david-bitner@hotmail.com>
2020-08-03 08:00:06 -0300

    Create Ex083Listas6.py

---
+++ Curso/ExMundo3/Ex083Listas6.py
@@ -0,0 +1,25 @@
+expressao = str(input('Digite a expressão: '))
+abreParenteses = 0
+fechaParenteses = 0
+primeiro = False
+
+for c in expressao:
+    if c in '(':
+        break
+
+    if c in ')':
+        primeiro = True
+        break
+
+if not primeiro:
+    for c in expressao:
+        if c in '(':
+            abreParenteses += 1
+
+        if c in ')':
+            fechaParenteses += 1
+
+if primeiro or abreParenteses != fechaParenteses:
+    print('A expressão está incorreta')
+else:
+    print('A expressão está correta')
