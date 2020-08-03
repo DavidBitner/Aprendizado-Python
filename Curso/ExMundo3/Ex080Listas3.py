a034f964bbfacd7580e9ed751af8222b05db046d
David <david-bitner@hotmail.com>
2020-08-03 08:47:36 -0300

    Update Ex080Listas3.py

--- Curso/ExMundo3/Ex080Listas3.py
+++ Curso/ExMundo3/Ex080Listas3.py
@@ -1,7 +1,18 @@
 lista = []
-novaLista = []
 
 for cont in range(0, 5):
-    n = int(input('Digite'))
+    n = int(input('Digite um valor: '))
 
-INCOMPLETO
+    if cont == 0 or n > lista[-1]:
+        lista.append(n)
+        print('Valor adicionado ao fim da lista...')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Valor adicionado a posição {pos} da lista...')
+                break
+            pos += 1
+
+print(f'Os valores digitados foram {lista}')
