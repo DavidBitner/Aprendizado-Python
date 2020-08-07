ff8546b50a8df4b02dd8662bb00f8b173f0c2217
David <david-bitner@hotmail.com>
2020-08-07 06:09:34 -0300

    Update Ex093Dicionarios4Futebol.py

--- Curso/ExMundo3/Ex093Dicionarios4Futebol.py
+++ Curso/ExMundo3/Ex093Dicionarios4Futebol.py
@@ -6,9 +6,7 @@ partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for c in range(0, partidas):
     gols.append(int(input(f'Quantos gols na partida {c + 1}? ')))
 jogador['gols'] = gols.copy()
-jogador['total'] = 0
-for c in gols:
-    jogador['total'] += c
+jogador['total'] = sum(gols)
 
 print('_' * 50)
 print(jogador)
