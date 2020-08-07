05944b8867762e8e7d6ddfabc8fb983a72826d67
David <david-bitner@hotmail.com>
2020-08-07 05:56:31 -0300

    Update Ex091Dicionarios2Dados.py

--- Curso/ExMundo3/Ex091Dicionarios2Dados.py
+++ Curso/ExMundo3/Ex091Dicionarios2Dados.py
@@ -1,23 +1,21 @@
 from random import randint
 from time import sleep
-jogadores = []
-jogada = {}
-
-for c in range(0, 4):
-    jogada['jogador'] = c + 1
-    jogada['jogada'] = randint(1, 6)
-    jogadores.append(jogada.copy())
-    jogada.clear()
+from operator import itemgetter
+jogada = {'jogador1': randint(1, 6),
+          'jogador2': randint(1, 6),
+          'jogador3': randint(1, 6),
+          'jogador4': randint(1, 6)}
+ranking = {}
 
 print('Valores sorteados:')
-for c in range(0, 4):
-    print(f'    O jogador{jogadores[c]["jogador"]} tirou {jogadores[c]["jogada"]}')
+for k, v in jogada.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(0.3)
 
-jogadores.sort()
+ranking = sorted(jogada.items(), key=itemgetter(1), reverse=True)
 
-print('Ranking dos jogadores:')
-for c in range(0, 4):
-    print(f'    {c}º lugar: {jogadores[c]["jogador"]} com {jogadores[c]["jogada"]}')
-
-
-INCOMPLETO
+print('-' * 50)
+print('== RANKING DOS JOGADORES ==')
+for c1, c2 in enumerate(ranking):
+    print(f'{c1 + 1}º lugar: {c2[0]} com {c2[1]}.')
+    sleep(0.3)
