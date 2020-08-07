ab49b0bf6644ed9ab2c064991a406b562dbb0f15
David <david-bitner@hotmail.com>
2020-08-07 06:05:03 -0300

    Update Ex092Dicionarios3.py

--- Curso/ExMundo3/Ex092Dicionarios3.py
+++ Curso/ExMundo3/Ex092Dicionarios3.py
@@ -11,11 +11,5 @@ if dicionario['carteira'] > 0:
     dicionario['salario'] = float(input('Salário: R$'))
     dicionario['aposentadoria'] = dicionario['contratacao'] - nascimento + 35
 
-print(dicionario)
-print(f'nome tem o valor {dicionario["nome"]}')
-print(f'idade tem o valor {dicionario["idade"]}')
-print(f'ctps tem o valor {dicionario["carteira"]}')
-if dicionario['carteira'] > 0:
-    print(f'contratação tem o valor {dicionario["contratacao"]}')
-    print(f'salario tem o valor {dicionario["salario"]}')
-    print(f'aposentadoria tem o valor {dicionario["aposentadoria"]}')
+for k, v in dicionario.items():
+    print(f'    - {k} tem o valor {v}')
