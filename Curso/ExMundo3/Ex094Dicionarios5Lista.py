7ed2dfd99380ba819e0a35f1d8d0601a4470999e
David <david-bitner@hotmail.com>
2020-08-07 06:25:18 -0300

    Update Ex094Dicionarios5Lista.py

--- Curso/ExMundo3/Ex094Dicionarios5Lista.py
+++ Curso/ExMundo3/Ex094Dicionarios5Lista.py
@@ -1,6 +1,5 @@
 pessoas = list()
 pessoa = dict()
-mulheres = list()
 contador = 0
 media = 0
 
@@ -14,9 +13,6 @@ while True:
 
     pessoa['idade'] = int(input('Idade: '))
     media += pessoa['idade']
-
-    if pessoa['sexo'] in 'Ff':
-        mulheres.append(pessoa['nome'])
 
     pessoas.append(pessoa.copy())
 
@@ -39,19 +35,12 @@ print(f'O grupo tem {len(pessoas)} pessoas.')
 print(f'A média de idade é de {media:.2f}')
 print(f'As mulheres cadastradas foram: ', end='')
 
-for c in mulheres:
-    print(c, end=' ')
+for c in pessoas:
+    if c['sexo'] in 'Ff':
+        print(c['nome'], end=' ')
 print(f'\nLista das pessoas que estão acima da média:\n')
 
-for c1 in range(0, len(pessoas)):
-    if pessoas[c1]['idade'] > media:
-        for k, v in pessoas[c1].items():
+for c in pessoas:
+    if c['idade'] > media:
+        for k, v in c.items():
             print(f'{k} = {v}', end='; ')
-        print('\n')
-
-'''
-for k, v in dicionario.items():
-    print(f'O {k} é {v}')
-= O nome é Pedro
-O idade é 25
-O sexo é m'''
