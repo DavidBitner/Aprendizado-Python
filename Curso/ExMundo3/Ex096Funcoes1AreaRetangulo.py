c0df132c447179546b34308d69ba7e4910c1d8e3
David <david-bitner@hotmail.com>
2020-08-09 10:15:04 -0300

    Update Ex096Funcoes1AreaRetangulo.py

--- Curso/ExMundo3/Ex096Funcoes1AreaRetangulo.py
+++ Curso/ExMundo3/Ex096Funcoes1AreaRetangulo.py
@@ -1,6 +1,11 @@
 def areaRetangulo(largura, comprimento):
     area = largura * comprimento
-    print(f'A área de um terreno {largura:.1f}X{comprimento:.1f} é de {area:.1f}.')
+    print(f'A área de um terreno {largura:.1f} X {comprimento:.1f} é de {area:.1f}m².')
 
 
-#Pro
+# Programa principal
+print('Controle de Terrenos')
+print('-' * 30)
+larg = float(input('LARGURA (m): '))
+comp = float(input('COMPRIMENTO (m): '))
+areaRetangulo(larg, comp)
