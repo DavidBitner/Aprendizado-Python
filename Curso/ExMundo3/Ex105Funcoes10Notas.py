a82882e34dd318efd51bfa02c6f8419b403fab63
David <david-bitner@hotmail.com>
2020-08-10 14:43:27 -0300

    Update Ex105Funcoes10Notas.py

--- Curso/ExMundo3/Ex105Funcoes10Notas.py
+++ Curso/ExMundo3/Ex105Funcoes10Notas.py
@@ -1,9 +1,22 @@
 def notas(*nota, sit=False):
+    """
+    Dicionario com as informações de um aluno
+    :param nota: Notas do aluno
+    :param sit: Parâmetro para apontar se o dicionário terá a situação do aluno
+    :return: Um dicionário com a quantidade de notas do aluno, a maior nota, a menor, a média, e caso necessário a situação do aluno.
+    """
     dicionario = dict()
     dicionario['total'] = len(nota)
     dicionario['maior'] = max(nota)
     dicionario['menor'] = min(nota)
     dicionario['media'] = sum(nota) / len(nota)
+    if sit:
+        if dicionario['media'] < 5:
+            dicionario['situação'] = 'RUIM'
+        elif dicionario['media'] < 7:
+            dicionario['situação'] = 'RAZOAVEL'
+        else:
+            dicionario['situação'] = 'BOA'
     return dicionario
 
 
