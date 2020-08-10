4d94c408b51e600d3be0a52ab2f17d2427aa5a85
David <david-bitner@hotmail.com>
2020-08-10 13:40:25 -0300

    Create Ex101Funcoes6Voto.py

---
+++ Curso/ExMundo3/Ex101Funcoes6Voto.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+
+def voto(ano):
+    idade = datetime.today().year - ano
+    if idade < 16:
+        print(f'Com {idade} anos: NÃO VOTA')
+    elif idade < 18 or idade > 65:
+        print(f'Com {idade} anos: VOTO OPCIONAL')
+    else:
+        print(f'Com {idade} anos: VOTO OBRIGATÓRIO')
+
+
+# Programa Principal
+voto(int(input('Em que ano você nasceu? ')))
