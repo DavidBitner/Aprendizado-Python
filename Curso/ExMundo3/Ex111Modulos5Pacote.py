e67f0a329a912a5eec3d9b8fb4c8c8e65e23477f
David <david-bitner@hotmail.com>
2020-08-12 11:49:01 -0300

    Refactor Ex111

--- Curso/ExMundo3/Ex111Modulos5Pacote.py
+++ Curso/ExMundo3/Ex111Modulos5Pacote.py
@@ -1,4 +1,4 @@
-from ExMundo3.Ex111Ex112.moeda import Ex111Modulo
+from ExMundo3.Ex111Ex112.moeda import Ex111Ex112Modulo
 
 p = float(input('Digite o preço: R$'))
-Ex111Modulo.resumo(p, 80, 35)
+Ex111Ex112Modulo.resumo(p, 80, 35)
