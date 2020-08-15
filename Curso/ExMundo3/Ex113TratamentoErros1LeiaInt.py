43002b652a40c27b0cd6607d704e23ddd9a7b48e
David <david-bitner@hotmail.com>
2020-08-15 20:24:23 -0300

    Create Ex113TratamentoErros1LeiaInt.py

---
+++ Curso/ExMundo3/Ex113TratamentoErros1LeiaInt.py
@@ -0,0 +1,27 @@
+def leiaInt(txt):
+    while True:
+        try:
+            numero = input(txt)
+        except Exception:
+            print('ERRO: por favor, digite um numero inteiro válido.')
+        else:
+            break
+    return numero
+
+
+def leiaFloat(txt):
+    while True:
+        try:
+            numero = float(input(txt))
+        except Exception:
+            print('ERRO: por favor, digite um numero Float válido.')
+        else:
+            break
+    return numero
+
+
+# Programa Principal
+i = leiaInt('Digite um numero inteiro: ')
+print(f'Você acabou de digitar o numero {i}')
+f = leiaFloat('Digite um numero float: ')
+print(f'Você acabou de digitar o float {f}')
