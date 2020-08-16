e34095a1f097f18aa2fa3e23bc9298ef89d8b885
David <david-bitner@hotmail.com>
2020-08-15 22:27:06 -0300

    Update Ex114TratamentoErros2Pudim.py

--- Curso/ExMundo3/Ex114TratamentoErros2Pudim.py
+++ Curso/ExMundo3/Ex114TratamentoErros2Pudim.py
@@ -1,1 +1,8 @@
-INCOMPLETO
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site pudim não está acessível no momento')
+else:
+    print('O site pudim pode ser acessado no momento')
