36f335577ca312d776e3f41a5bd736be89d63adb
David <david-bitner@hotmail.com>
2020-08-19 17:19:54 -0300

    Create Anotações.py

---
+++ Curso/ModuloTkinter/Anotações.py
@@ -0,0 +1,9 @@
+'''
+Inicialização do pacote sempre será feita pelo comando Tk(). Exemplo:
+janela = Tk()
+
+E para fazer o comando funcionar, sempre haverá o comando mainloop(). Exemplo
+janela.mainloop()
+
+
+'''
