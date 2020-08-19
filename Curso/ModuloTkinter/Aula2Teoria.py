fbc7c0a66522291b07f1c2a8855383797554c68a
David <david-bitner@hotmail.com>
2020-08-19 17:32:36 -0300

    Create Aula2Teoria.py

---
+++ Curso/ModuloTkinter/Aula2Teoria.py
@@ -0,0 +1,14 @@
+# Aula dois foi composta apenas de teoria
+
+'''
+TkInter == TK Interface
+TkInter é uma biblioteca gráfica nativa do Python
+A bibliotexa é um binding, ou seja, a biblioteca vem com o Python, mas não faz parte do desenvolvimento Python
+GUI = Graphic User Interface
+widget é todo componente que compõe uma interface gráfica, como botões, botões de opção, campos de texto, janelas e rótulos de texto.
+container é um widget que pode conter outros widgets. Todo container também está contido em outro container, salvo a janela principal
+window é um termo que possui significados diferentes em contextos diferentes. Em geral, se refere a uma área retangular em algum lugar na tela de exibição.
+top-level window é uma janela independente e que normalmente está sendo exibida sob as demais
+frame é a unidade básica de organização de layouts complexos.
+child-parent é o nome da relação entre um widget e seu container. Um campo adicionado a uma janela é uma relação parent-child, onde a janela é o parent e o campo o child.
+'''
