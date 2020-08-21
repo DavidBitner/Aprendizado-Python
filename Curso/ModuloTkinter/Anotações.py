bbe7ec4024003767f041f81dda66ad9285bb785e
David <david-bitner@hotmail.com>
2020-08-20 23:03:51 -0300

    Update Anotações.py

--- Curso/ModuloTkinter/Anotações.py
+++ Curso/ModuloTkinter/Anotações.py
@@ -1,34 +1,42 @@
 '''
-TkInter == TK Interface
+TkInter = TK Interface
 TkInter é uma biblioteca gráfica nativa do Python
 A bibliotexa é um binding, ou seja, a biblioteca vem com o Python, mas não faz parte do desenvolvimento Python
 GUI = Graphic User Interface
-widget é todo componente que compõe uma interface gráfica, como botões, botões de opção, campos de texto, janelas e rótulos de texto.
-container é um widget que pode conter outros widgets. Todo container também está contido em outro container, salvo a janela principal
-window é um termo que possui significados diferentes em contextos diferentes. Em geral, se refere a uma área retangular em algum lugar na tela de exibição.
-top-level window é uma janela independente e que normalmente está sendo exibida sob as demais
-frame é a unidade básica de organização de layouts complexos.
-child-parent é o nome da relação entre um widget e seu container. Um campo adicionado a uma janela é uma relação parent-child, onde a janela é o parent e o campo o child.
+widget = todo componente que compõe uma interface gráfica, como botões, botões de opção, campos de texto, janelas e rótulos de texto.
+container = um widget que pode conter outros widgets. Todo container também está contido em outro container, salvo a janela principal
+window = um termo que possui significados diferentes em contextos diferentes. Em geral, se refere a uma área retangular em algum lugar na tela de exibição.
+top-level window = uma janela independente e que normalmente está sendo exibida sob as demais
+frame = a unidade básica de organização de layouts complexos.
+child-parent = o nome da relação entre um widget e seu container. Um campo adicionado a uma janela é uma relação parent-child, onde a janela é o parent e o campo o child.
 
 
-Inicialização do pacote sempre será feita pelo comando Tk(). Exemplo:
-janela = Tk()
+janela = Tk() = Inicialização do pacote sempre será feita pelo comando Tk(). Exemplo:
 
-Para fazer o comando funcionar, sempre haverá o comando mainloop(). Exemplo
-janela.mainloop()
+janela.mainloop() = Para fazer o comando funcionar, sempre haverá o comando mainloop().
 
 
-Para atribuir um titulo a uma janela usa-se o comando .title()
-Para atribuir uma cor de background para uma janela, usa-se a propriedade "bg". Exemplo: janela['bg'] = 'green'
-Para atribuir a posição de algo usa-se o comando .geometr(). Tendo como lógica LarguraxAltura+Esquerda+Topo. Exemplo: 300x300+100+100
+.title() = Para atribuir um titulo a uma janela usa-se o comando
+janela['bg'] = 'green' = Para atribuir uma cor de background para uma janela, usa-se a propriedade "bg".
+.geometr()00x300+100+100) = Para atribuir a posição de algo usa-se o comando . Tendo como lógica LarguraxAltura+Esquerda+Topo.
 Caso apenas seja necessário definir o tamanho em largura e altura, basta usar LarguraxAltura.
 Caso apenas seja necessário definir a distância da janela da esquerda e do topo da tela, basta usar +Esquerda+Topo
 
+.grid(row=0, column=0) = Para posicionar algo na tela, uma das maneiras de se fazer isso é através da função .grid, onde são apontadas as linhas e colunas do grid para posicionar os widgets do programa.
 
-Os três pilares do desenvolvimento de software gui são:
-1- Gerenciadores de layout, ou a forma que os objetos de uma tela serão organizados
-2- Widgets, ou os elementos da tela, tudo que aparecer nela, desde botões até formulários e campos de texto
-3- Eventos, ou tudo que for uma função, tudo que for criado com o objetivo de executar uma função é um evento
+Button() = Função para criar um botão
+Button(state=disabled) = state serve para determinar o estado do botão, nesse caso desabilitado
+Button(padx=50, pady=50) = Determinar o tamanho do botão
+Button(command=exemplo) = Passar a função que o comando ira executar
+Button(fg="white", bg="black") = Definir a cor do que estiver dentro do botão como branca, e o background do botão como preto
 
-A ideia é que os widgets sirvam de elementos visuais para os eventos que forem criados no programa
+
+Entry() = Função para uma entrada, ou um campo de texto
+Entry(width=50) = Apontar o tamanho do widget
+Entry(borderwidth=5) = Determinar o tamanho da borda do campo
+exemplo.get() = Capturar o que estiver escrito no campo de texto
+exemplo.insert() = Inserir dentro do campo de texto algo assim que o programa for executado
+
+Para fazer um botão interagir com o campo de texto, basta fazer a função que o botão está executando interagir com o campo de texto
+
 '''
