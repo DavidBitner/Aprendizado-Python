ec7f23d82758c9a26fa08dde0dae951c7fadbfee
David <david-bitner@hotmail.com>
2020-08-20 22:00:20 -0300

    Update Anotações.py

--- Curso/ModuloTkinter/Anotações.py
+++ Curso/ModuloTkinter/Anotações.py
@@ -25,4 +25,10 @@ Caso apenas seja necessário definir o tamanho em largura e altura, basta usar L
 Caso apenas seja necessário definir a distância da janela da esquerda e do topo da tela, basta usar +Esquerda+Topo
 
 
+Os três pilares do desenvolvimento de software gui são:
+1- Gerenciadores de layout, ou a forma que os objetos de uma tela serão organizados
+2- Widgets, ou os elementos da tela, tudo que aparecer nela, desde botões até formulários e campos de texto
+3- Eventos, ou tudo que for uma função, tudo que for criado com o objetivo de executar uma função é um evento
+
+A ideia é que os widgets sirvam de elementos visuais para os eventos que forem criados no programa
 '''
