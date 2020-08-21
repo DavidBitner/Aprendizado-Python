c15b0fcb39bfad126610bfd0345d41cea9e73fd5
David <david-bitner@hotmail.com>
2020-08-20 21:55:19 -0300

    Create Aula4TresPilares.py

---
+++ Curso/ModuloTkinter/Aula4TresPilares.py
@@ -0,0 +1,10 @@
+# Aula composta apenas de teoria
+
+'''
+Os três pilares do desenvolvimento de software gui são:
+1- Gerenciadores de layout, ou a forma que os objetos de uma tela serão organizados
+2- Widgets, ou os elementos da tela, tudo que aparecer nela, desde botões até formulários e campos de texto
+3- Eventos, ou tudo que for uma função, tudo que for criado com o objetivo de executar uma função é um evento
+
+A ideia é que os widgets sirvam de elementos visuais para os eventos que forem criados no programa
+'''
