50fd6b2477eb7514990bc79735f1b6a3b916d6c0
David <david-bitner@hotmail.com>
2020-08-23 01:47:16 -0300

    Create CodelandUsernameValidation.py

---
+++ Curso/Challenges/CoderByte/CodelandUsernameValidation.py
@@ -0,0 +1,13 @@
+def CodelandUsernameValidation(strParam):
+    if "_" in strParam and strParam[-1] not in "_":
+        strParam = strParam.replace("_", "a")
+    if strParam.isalnum() and strParam[0].isalpha() and strParam[-1] not in "_" and 3 < len(strParam) < 26:
+        strParam = "true"
+    else:
+        strParam = "false"
+    # code goes here
+    return strParam
+
+
+# keep this function call here
+print(CodelandUsernameValidation(input()))
