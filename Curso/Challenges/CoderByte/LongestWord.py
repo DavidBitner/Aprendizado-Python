8054d801aa9b0dd19f32cbebb5b4c50fa7a688ed
David <david-bitner@hotmail.com>
2020-08-23 02:02:54 -0300

    Create LongestWord.py

---
+++ Curso/Challenges/CoderByte/LongestWord.py
@@ -0,0 +1,17 @@
+import string
+
+
+def LongestWord(sen):
+    longest = ""
+    for c in string.punctuation:
+        sen = sen.replace(c, "")
+    sen = sen.split()
+    for word in sen:
+        if len(word) > len(longest):
+            longest = word
+    sen = longest
+    return sen
+
+
+# keep this function call here
+print(LongestWord(input()))
