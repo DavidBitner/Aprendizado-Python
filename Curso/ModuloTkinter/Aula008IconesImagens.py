28b8226aa581487d0a20a32f15112f5c8ec85220
David <david-bitner@hotmail.com>
2020-08-24 04:35:13 -0300

    Create Aula008IconesImagens

---
+++ Curso/ModuloTkinter/Aula008IconesImagens.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title("Doidera")
+root.iconbitmap("Aula008icone.ico")
+
+my_img = ImageTk.PhotoImage(Image.open("Aula008Imagem.png"))
+my_label = Label(image=my_img)
+my_label.pack()
+
+button_quit = Button(root, text="SAIR", command=root.quit)
+button_quit.pack()
+
+root.mainloop()
