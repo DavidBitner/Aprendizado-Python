495acf55bdb3745aa4e1e2cc9d9895b5e45c9aa6
David <david-bitner@hotmail.com>
2020-08-28 00:02:23 -0300

    Update Aula011Frames.py

--- Curso/ModuloTkinter/Aula011Frames.py
+++ Curso/ModuloTkinter/Aula011Frames.py
@@ -3,6 +3,13 @@ from tkinter import *
 root = Tk()
 root.title("Doidera")
 
+frame = LabelFrame(root, text="This is my frame...", padx=5, pady=5)
+frame.pack(padx=100, pady=100)
 
+my_button1 = Button(frame, text="DON'T CLICK HERE")
+my_button2 = Button(frame, text="UAU")
+
+my_button1.grid(row=0, column=0)
+my_button2.grid(row=1, column=1)
 
 root.mainloop()
