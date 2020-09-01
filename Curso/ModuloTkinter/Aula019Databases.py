d562fe4058961a81ce6fa9aee9685f7b1462624b
David <david-bitner@hotmail.com>
2020-09-01 07:05:42 -0300

    Update Aula019Databases.py

--- Curso/ModuloTkinter/Aula019Databases.py
+++ Curso/ModuloTkinter/Aula019Databases.py
@@ -6,7 +6,7 @@ root.title("Doidera")
 root.geometry("400x400+200+200")
 
 # Criar o banco de dados ou conectar a um
-conexao = sqlite3.connect("Aula019Banco.db")
+conexao = sqlite3.connect("Aula019...Banco.db")
 
 # Criar cursor
 cursor = conexao.cursor()
