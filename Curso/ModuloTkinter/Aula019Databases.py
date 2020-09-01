4cb014ba98e090b48d70254672221d791ccbc3ec
David <david-bitner@hotmail.com>
2020-09-01 07:02:39 -0300

    Create Aula019Databases.py

---
+++ Curso/ModuloTkinter/Aula019Databases.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import sqlite3
+
+root = Tk()
+root.title("Doidera")
+root.geometry("400x400+200+200")
+
+# Criar o banco de dados ou conectar a um
+conexao = sqlite3.connect("Aula019Banco.db")
+
+# Criar cursor
+cursor = conexao.cursor()
+
+# Criar tabela
+cursor.execute("""CREATE TABLE enderecos (
+primeiro_nome text,
+sobrenome text,
+endereco text,
+cidade text,
+estado text,
+cep integer)""")
+
+# Commitar mudanças
+conexao.commit()
+
+# Fechar conexão
+conexao.close()
+
+root.mainloop()
