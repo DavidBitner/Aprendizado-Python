318c0b1d8439331e8977c07a17dab7c39e74369a
David <david-bitner@hotmail.com>
2020-09-03 07:00:21 -0300

    Create 1041CoordinatesOfAPoint.py

---
+++ Curso/Challenges/URI/1041CoordinatesOfAPoint.py
@@ -0,0 +1,16 @@
+A, B = input().split(" ")
+x, y = float(A), float(B)
+if x > 0 and y > 0:
+    print("Q1")
+elif x > 0 > y:
+    print("Q4")
+elif x < 0 < y:
+    print("Q2")
+elif x < 0 and y < 0:
+    print("Q3")
+elif x == 0 and y != 0:
+    print("Eixo Y")
+elif x != 0 and y == 0:
+    print("Eixo X")
+else:
+    print("Origem")
