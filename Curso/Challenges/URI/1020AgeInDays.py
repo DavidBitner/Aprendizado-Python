71af176d9455df29d20d82ae9badf5806910ef40
David <david-bitner@hotmail.com>
2020-09-10 14:21:35 -0300

    Update 1020AgeInDays.py

--- Curso/Challenges/URI/1020AgeInDays.py
+++ Curso/Challenges/URI/1020AgeInDays.py
@@ -1,11 +1,9 @@
 person_age_in_days = int(input())
-days = person_age_in_days % 30 // 2
-temp = person_age_in_days // 30
-months = temp % 12
-temp = temp // 12
-years = temp
+years = person_age_in_days // 365
+temp = person_age_in_days % 365
+months = temp // 30
+temp = temp % 30
+days = temp
 print(f"{years} ano(s)")
 print(f"{months} mes(es)")
 print(f"{days} dia(s)")
-
-INCOMPLETO
