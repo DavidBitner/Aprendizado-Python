15828b3860928893532394321959f6168bce1ff9
David <david-bitner@hotmail.com>
2020-09-21 22:49:28 -0300

    Create 1133RestOfADivision.py

---
+++ Curso/Challenges/URI/1133RestOfADivision.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+maior = menor = 0
+if x > y:
+    maior = x
+    menor = y
+else:
+    maior = y
+    menor = x
+for i in range(menor + 1, maior):
+    if i % 5 == 2 or i % 5 == 3:
+        print(i)
