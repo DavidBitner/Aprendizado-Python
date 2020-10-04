4dd459430a262d98cab0389949e475797be527f1
David <david-bitner@hotmail.com>
2020-10-04 05:42:18 -0300

    Create 1185AboveTheSecundaryDiagonal.py

---
+++ Curso/Challenges/URI/1185AboveTheSecundaryDiagonal.py
@@ -0,0 +1,16 @@
+operacao = str(input())
+soma = media = contador = 0
+matriz = [[], [], [], [], [], [], [], [], [], [], [], []]
+for linha in range(0, 12):
+    for coluna in range(0, 12):
+        n = float(input())
+        matriz[linha].append(n)
+for linha in range(0, 12):
+    for coluna in range(0, 12):
+        if coluna < 11 - linha:
+            soma += matriz[coluna][linha]
+            contador += 1
+if operacao in "S":
+    print(f"{soma:.1f}")
+else:
+    print(f"{soma / contador:.1f}")
