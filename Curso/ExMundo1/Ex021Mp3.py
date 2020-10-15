7c3e2c42071489243e3731fe1b33b38797dbd596
David <david-bitner@hotmail.com>
2020-10-15 11:07:56 -0300

    Update Ex021Mp3.py

--- Curso/ExMundo1/Ex021Mp3.py
+++ Curso/ExMundo1/Ex021Mp3.py
@@ -4,4 +4,8 @@ pygame.mixer.init()
 pygame.init()
 pygame.mixer.music.load('C:/Users/David/Music/Musicas/Curtains/Feint - The Things Weve Seen.mp3')
 pygame.mixer.music.play()
-input('Aperte uma tecla para parar')
+while True:
+    a = int(input())
+    if a == 1:
+        pygame.mixer.music.stop()
+        break
